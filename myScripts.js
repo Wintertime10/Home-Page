1ec9f781e7f45ad6b3ae5436b544092bf16aa97b
Wintertime10 <159956992+Wintertime10@users.noreply.github.com>
2024-11-07 08:15:17 -0600

    fs
    dsaad

--- myScripts.js
+++ myScripts.js
@@ -14,3 +14,25 @@ function checkTime(i) {
    return i;
 }
   
+
+
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+ }
+  
+ function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+ }
+ var show = false;
+function showNote(){
+   if (show == false){
+       document.getElementById("note").style.opacity = 1;
+       show = true;
+   }
+   else if (show == true){
+       document.getElementById("note").style.opacity = 0;
+       show = false;
+   }
+}
